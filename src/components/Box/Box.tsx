9a4839d0c813fbc58f720bbdcabcd1be02880767
tolstovr <tolstovrob@gmail.com>
2024-08-01 20:50:23 +0400

    Fix (Box): Box works correct with images
    Besides background-image fix, there're changed border-radius and padding from 1rem to 1.5rem

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -11,8 +11,9 @@ interface BoxProps {
 
 const Box = ({ backgroundURL, backgroundColor, gridArea, className, children }: BoxProps) => {
   return (
-    <div className={`${styles.box} ${className ?? ""}`} style={{
-      ...(backgroundURL ? {background: backgroundURL} : backgroundColor ? {background: backgroundColor} : {}),
+    <div className={`${styles.box} ${className ?? ""} ${backgroundURL && styles["bg-img"]}`} style={{
+      ...(backgroundURL && {backgroundImage: `url(${backgroundURL})`}),
+      ...(backgroundColor && {background: backgroundColor}),
       gridArea: gridArea }}>
       {children}
     </div>
