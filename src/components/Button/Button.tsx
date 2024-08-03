fe2cec3ad1a2b9f1f14cd6c1823ff067edbcb8c0
tolstovr <tolstovrob@gmail.com>
2024-08-03 09:40:50 +0400

    Feat (Button): Initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import styles from "./Button.module.scss"
+
+interface ButtonProps {
+  variant?: "fill" | "outline"
+  size?: "small" | "medium" | "large"
+  color?: "default" | "primary" | "secondary" | "error" | "success" | "warning"
+  onClick?: () => void
+  icon?: React.ReactNode
+  disabled?: boolean
+  fullwidth?: boolean
+  round?: boolean
+  children?: React.ReactNode
+}
+
+const Button = ({ children, ...props }: ButtonProps) => {
+  console.log(styles)
+  return (
+    <button className={`${styles.button} ${props.variant ? styles[props.variant] : styles.fill} ${props.size ? styles[props.size] : styles.medium} ${props.fullwidth && styles.fullwidth } ${props.round && styles.round} ${props.color ? styles[props.color] : styles.default}`} disabled={props.disabled} onClick={props.onClick}>
+        {props.icon && <span className={styles.icon}>{props.icon}</span>}
+        {children}
+    </button>
+  )
+}
+
+export default Button
