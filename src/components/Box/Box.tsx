ca1fff934829888f6882e1fe2f5aa71c3e3c7e8a
tolstovr <tolstovrob@gmail.com>
2024-08-04 08:20:11 +0400

    Feat (Box): Added scroll animation support to Box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,22 +1,74 @@
-import React from "react"
+"use client"
+import React, { useEffect } from "react"
+import { motion, useAnimation, backOut } from "framer-motion"
+import { useInView } from "react-intersection-observer"
 import styles from "./Box.module.scss"
 
 interface BoxProps {
   backgroundURL?: string
   backgroundColor?: string
   gridArea: string
+  animation?: "fade-up" | "fade-left" | "fade-right" | "fade-down" | "scale-in" | "scale-out"
+  delay?: number
   className?: string
   children?: React.ReactNode
 }
 
-const Box = ({ backgroundURL, backgroundColor, gridArea, className, children }: BoxProps) => {
-  return (
-    <div className={`${styles.box} ${className ?? ""} ${backgroundURL && styles["bg-img"]}`} style={{
+const variants = {
+  "fade-up": {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 }
+  },
+  "fade-left": {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 }
+  },
+  "fade-right": {
+    hidden: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0 }
+  },
+  "fade-down": {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 }
+  },
+  "scale-in": {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 }
+  },
+  "scale-out": {
+    hidden: { opacity: 0, scale: 1.5 },
+    visible: { opacity: 1, scale: 1 }
+  }
+}
+
+const Box = ({ backgroundURL, backgroundColor, gridArea, animation, delay = 0, className, children }: BoxProps) => {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible")
+    }
+  }, [controls, inView])
+
+  const divProps = {
+    className: `${styles.box} ${className ?? ""} ${backgroundURL && styles["bg-img"]}`,
+    style: {
       ...(backgroundURL && {backgroundImage: `url(${backgroundURL})`}),
       ...(backgroundColor && {background: backgroundColor}),
-      gridArea: gridArea }}>
-      {children}
-    </div>
+      gridArea: gridArea
+    },
+    ...(animation && {
+      ref: ref,
+      initial: "hidden",
+      animate: controls,
+      variants: variants[animation],
+      transition: { delay, duration: 0.25, backOut }
+    })
+  }
+
+  return (
+    animation ? <motion.div {...divProps}>{children}</motion.div> : <div {...divProps}>{children}</div>
   )
 }
 
