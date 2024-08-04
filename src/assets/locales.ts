380f074e2b8f0cf0d33c711345efea7e2723d855
tolstovr <tolstovrob@gmail.com>
2024-08-04 09:57:12 +0400

    Feat (Locales): Added simple localisation via JSON

---
+++ src/assets/locales.ts
@@ -0,0 +1,29 @@
+export const ru = {
+  hero: {
+    title: "Привет, я Роберт!",
+    info: "Я frontend разработчик и UI/UX инженер. Занимаюсь разработкой веб-приложений, преподавательской деятельностью, а ещё люблю играть в шахматы. А это мой сайт портфолио. Добро пожаловать!",
+    cv: "Резюме",
+    tgchannel: "TG канал",
+    vk: "ВК",
+    tg: "Телеграм",
+    github: "Github",
+    gmail: "Написать в Gmail",
+    behance: "Behance",
+    lichess: "Сыграть со мной в шахматы"
+  }
+}
+
+export const en = {
+  hero: {
+    title: "Hi, I'm Robert!",
+    info: "I'm frontend developer and UI/UX engineer. I develop modern web applications, teach students and play some chess. And BTW, this is my portfolio website. Welcome!",
+    cv: "Get CV",
+    tgchannel: "TG channel",
+    vk: "VK",
+    tg: "Telegram",
+    github: "Github",
+    gmail: "Contact via Gmail",
+    behance: "Behance",
+    lichess: "Play chess with me"
+  }
+}
