5bf7096f7a310b8c28fa1009679a41d63f9fd414
tolstovr <tolstovrob@gmail.com>
2024-08-04 19:35:39 +0400

    Markup: Added Theme and Lang providers to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 import React from "react"
+import { ThemeProvider } from "@/hooks/ThemeContext"
+import { LocaleProvider } from "@/hooks/LocaleContext"
 import HeroSection from "@/modules/HeroSection/HeroSection"
 import styles from "./page.module.scss"
 
 const Home = () => {
   return (
-    <main>
-      <HeroSection />
-    </main>
+    <LocaleProvider>
+      <ThemeProvider>
+        <main>
+          <HeroSection />
+        </main>
+    </ThemeProvider>
+    </LocaleProvider>
   )
 }
 
