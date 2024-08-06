76ab80c8ba98b0fac34932482bcfb086f452142c
tolstovr <tolstovrob@gmail.com>
2024-08-06 20:09:57 +0400

    Refactor: Shorten getRows and getColumns functions

--- src/utils/getRowsColumns.ts
+++ src/utils/getRowsColumns.ts
@@ -1,7 +1,2 @@
-export const getRows = (areas: string[]) => {
-  return areas.length
-}
-
-export const getColumns = (areas: string[]) => {
-  return areas.reduce((acc, area) => Math.max(acc, area.split(" ").length), 0)
-}
+export const getRows = (areas: string[]) => areas.length
+export const getColumns = (areas: string[]) => areas.reduce((acc, area) => Math.max(acc, area.split(" ").length), 0)
